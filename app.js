81f473f677c3b611e70ce21ac5dea9f5a850ac5a
Cody Fayolle <cfayolle@umich.edu>
2018-03-08 21:30:56 -0600

    Added Rules html

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require( 'express' );
 const app = express();
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
-app.get('/', function (req, res) {
-  res.render('index');
-  console.log('rendered');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use('/', routes);
+
+app.use(express.static("public"));
 
 app.listen(2007, function(){
   console.log('We out here');
